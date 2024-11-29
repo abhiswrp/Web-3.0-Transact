13eeb385e6946e436deb647515245c340c0ea98b
Abhishek Swaroop <abhi.swrp4321@gmail.com>
2024-11-29 23:04:30 +0530

    Resize

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 import WalletsSection from '../components/WalletsSection';
 import logo from '../../images/Abhishek_Swaroop_Sign.png';
 
@@ -19,25 +21,55 @@ NavbarItem.propTypes = {
 
 const Navbar = () => {
   const [showWallets, setShowWallets] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleWallets = () => setShowWallets(!showWallets);
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="flex justify-start items-center w-full">
-        <img src={logo} alt="logo" className="w-16 h-16 cursor-pointer" />
+        <img src={logo} alt="logo" className="w-17 h-16 cursor-pointer" />
       </div>
+
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         <NavbarItem title="Market" href="https://etherscan.io/" />
         <NavbarItem title="Exchange" href="https://exchange.example.com/" />
         <NavbarItem title="Tutorials" href="https://tutorials.example.com/" />
-        <li
-          className="mx-4 cursor-pointer hover:text-gray-300"
-          onClick={toggleWallets}
-        >
+        <li className="mx-4 cursor-pointer hover:text-gray-300" onClick={toggleWallets}>
           Wallets
         </li>
       </ul>
+
+      <div className="md:hidden flex items-center">
+        <HiMenuAlt4
+          fontSize={28}
+          className="text-white cursor-pointer"
+          onClick={toggleMobileMenu}
+        />
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="absolute top-20 right-0 text-white shadow-md rounded-md p-4 z-10 md:hidden flex flex-col items-start w-full">
+          <ul className="list-none">
+            <NavbarItem title="Market" href="https://etherscan.io/" />
+            <NavbarItem title="Exchange" href="https://exchange.example.com/" />
+            <NavbarItem title="Tutorials" href="https://tutorials.example.com/" />
+            <li
+              className="mx-4 cursor-pointer hover:text-gray-300"
+              onClick={toggleWallets}
+            >
+              Wallets
+            </li>
+          </ul>
+          <AiOutlineClose
+            fontSize={28}
+            className="absolute top-4 right-4 cursor-pointer"
+            onClick={toggleMobileMenu}
+          />
+        </div>
+      )}
+
       {showWallets && (
         <div className="absolute top-20 right-0 bg-gray-800 text-white shadow-md rounded-md p-4 z-10">
           <WalletsSection />
